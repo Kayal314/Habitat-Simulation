138e6b9fabaef6f4ffbf1dd381a8435f223719a8
Kayal314 <74356284+Kayal314@users.noreply.github.com>
2021-06-03 11:49:54 +0530

    method to decrease resources

--- landscape.py
+++ landscape.py
@@ -1,28 +1,18 @@
 class Landform:
     """
-    We define a landform as the 5-tuple (L, F, W, R, f), where:
+    We define a landform as the 3-tuple (L, F, W) where:
         L = locations
         F = food availability
         W = water availability
-        R = rabbit count
-        f = fatality
     """
 
-    def __init__(self, locations: list, food: float, water: float, rabbit_count: int, fatality: int):
-        self.locations = locations
+    def __init__(self, locations: list, food: float, water: float):
+        self.__locations = locations
         self.__food = food
         self.__water = water
-        self.__rabbit_count = rabbit_count
-        self.__fatality = fatality
 
-    def increment_rabbit_count(self):
-        self.__rabbit_count += 1
-
-    def decrement_rabbit_count(self):
-        self.__rabbit_count -= 1
-
-    def get_rabbit_count(self):
-        return self.__rabbit_count
+    def get_locations(self):
+        return self.__locations
 
     def get_water_amount(self):
         return self.__water
@@ -30,35 +20,46 @@ class Landform:
     def get_food_amount(self):
         return self.__food
 
-    def get_fatality(self):
-        return self.__fatality
 
 
 class Grassland(Landform):
     def __init__(self):
-       super(Grassland, self).__init__([(953, 113), (467, 383), (899,522),(945,888),(1676,276)], 500, 0, 0, 0)
+        super(Grassland, self).__init__([(953, 113), (467, 383), (899, 522), (945, 888), (1676, 276)], 500, 0)
+
+    def decrement_food(self):
+        self.__food -= 7
 
 
 class Forest(Landform):
     def __init__(self):
-       super(Forest, self).__init__([(348, 163), (1326, 259), (1705,586),(633,701),(290,856)], 500, 0, 0, 0)
+        super(Forest, self).__init__([(348, 163), (1326, 259), (1705, 586), (633, 701), (290, 856)], 400, 50)
+
+    def decrement_food(self):
+        self.__food -= 5
 
 
 class Lake(Landform):
     def __init__(self):
-       super(Lake, self).__init__([(1591, 878), (346,587)], 500, 0, 0, 0)
+        super(Lake, self).__init__([(1591, 878), (346, 587)], 0, 500)
+
+    def decrement_water(self):
+        self.__food -= 7
 
 
 class Pond(Landform):
     def __init__(self):
-       super(Pond, self).__init__([(487,198),(1330,68),(1438,487)], 500, 0, 0, 0)
+        super(Pond, self).__init__([(487, 198), (1330, 68), (1438, 487)], 0, 50)
+
+    def decrement_water(self):
+        self.__food -= 5
 
 
 class Quagmire(Landform):
     def __init__(self):
-       super(Quagmire, self).__init__([(98, 330), (1158, 533)], 500, 0, 0, 0)
+        super(Quagmire, self).__init__([(98, 330), (1158, 533)], 0, 0)
 
 
 class Rugged(Landform):
     def __init__(self):
-       super(Rugged, self).__init__([(923,266),(1438,659)], 500, 0, 0, 0)
+        super(Rugged, self).__init__([(923, 266), (1438, 659)], 0, 0)
+
