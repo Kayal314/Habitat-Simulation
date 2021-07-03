0ee7250c76eb331cf9ee8d9e5cdb1f9628ec98e9
Stinky Ball <74356284+Kayal314@users.noreply.github.com>
2021-07-03 16:44:22 +0530

    Final Commit

--- stats.py
+++ stats.py
@@ -1,5 +1,8 @@
+import math
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from scipy import stats
 
 
 def write_population(rabbit: list, fox: list, grassland: list, forest: list, pond: list, lake: list):
@@ -48,7 +51,7 @@ def write(grassland: list, forest: list, pond: list, lake: list, rabbit: list, f
     f.close()
 
 
-def find_genetic_variation_rabbit(rabbits: list) -> list:
+def find_genetic_variation_rabbit_std(rabbits: list) -> list:
     variance = []
     data = []
     for rabbit in rabbits:
@@ -77,7 +80,7 @@ def find_genetic_variation_rabbit(rabbits: list) -> list:
     return variance
 
 
-def find_genetic_variation_fox(foxes: list) -> list:
+def find_genetic_variation_fox_std(foxes: list) -> list:
     variance = []
     data = []
     for fox in foxes:
@@ -106,10 +109,314 @@ def find_genetic_variation_fox(foxes: list) -> list:
     return variance
 
 
-def plot_variance_rabbit(variance):
+def find_genetic_variation_rabbit_mean_ad(rabbits: list) -> list:
+    variance = []
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.mating_requirement)
+    variance.append(pd.Series(data).mad())
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.step_size)
+    variance.append(pd.Series(data).mad())
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.thirst_resistance)
+    variance.append(pd.Series(data).mad())
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.hunger_resistance)
+    variance.append(pd.Series(data).mad())
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.predator_fear)
+    variance.append(pd.Series(data).mad())
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.vision)
+    variance.append(pd.Series(data).mad())
+    return variance
+
+
+def find_genetic_variation_fox_mean_ad(foxes: list) -> list:
+    variance = []
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.mating_requirement)
+    variance.append(pd.Series(data).mad())
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.step_size)
+    variance.append(pd.Series(data).mad())
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.thirst_resistance)
+    variance.append(pd.Series(data).mad())
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.hunger_resistance)
+    variance.append(pd.Series(data).mad())
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.hunting_skill)
+    variance.append(pd.Series(data).mad())
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.vision)
+    variance.append(pd.Series(data).mad())
+    return variance
+
+
+def find_genetic_variation_rabbit_mad(rabbits: list) -> list:
+    variance = []
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.mating_requirement)
+    variance.append(stats.median_abs_deviation(data))
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.step_size)
+    variance.append(stats.median_abs_deviation(data))
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.thirst_resistance)
+    variance.append(stats.median_abs_deviation(data))
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.hunger_resistance)
+    variance.append(stats.median_abs_deviation(data))
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.predator_fear)
+    variance.append(stats.median_abs_deviation(data))
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.vision)
+    variance.append(stats.median_abs_deviation(data))
+    return variance
+
+
+def find_genetic_variation_fox_mad(foxes: list) -> list:
+    variance = []
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.mating_requirement)
+    variance.append(stats.median_abs_deviation(data))
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.step_size)
+    variance.append(stats.median_abs_deviation(data))
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.thirst_resistance)
+    variance.append(stats.median_abs_deviation(data))
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.hunger_resistance)
+    variance.append(stats.median_abs_deviation(data))
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.hunting_skill)
+    variance.append(stats.median_abs_deviation(data))
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.vision)
+    variance.append(stats.median_abs_deviation(data))
+    return variance
+
+
+def find_genetic_variation_rabbit_coeff_var(rabbits: list) -> list:
+    variance = []
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.mating_requirement)
+    variance.append(stats.variation(data))
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.step_size)
+    variance.append(stats.variation(data))
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.thirst_resistance)
+    variance.append(stats.variation(data))
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.hunger_resistance)
+    variance.append(stats.variation(data))
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.predator_fear)
+    variance.append(stats.variation(data))
+    data = []
+    for rabbit in rabbits:
+        data.append(rabbit.genetics.vision)
+    variance.append(stats.variation(data))
+    return variance
+
+
+def find_genetic_variation_fox_coeff_var(foxes: list) -> list:
+    variance = []
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.mating_requirement)
+    variance.append(stats.variation(data))
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.step_size)
+    variance.append(stats.variation(data))
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.thirst_resistance)
+    variance.append(stats.variation(data))
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.hunger_resistance)
+    variance.append(stats.variation(data))
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.hunting_skill)
+    variance.append(stats.variation(data))
+    data = []
+    for fox in foxes:
+        data.append(fox.genetics.vision)
+    variance.append(stats.variation(data))
+    return variance
+
+
+def find_genetic_variation_rabbit_entropy(rabbits: list) -> list:
+    variance = []
+    population = len(rabbits)
+    entropy = 0
+    l = np.linspace(0, 4, 20)
+    for j in range(19):
+        proportion = 0
+        for rabbit in rabbits:
+            if l[j] <= rabbit.genetics.mating_requirement < l[j + 1]:
+                proportion += 1
+        if proportion > 0:
+            entropy += -(proportion / population) * math.log2(proportion / population)
+    variance.append(entropy)
+    entropy = 0
+    l = np.linspace(15, 40, 20)
+    for j in range(19):
+        proportion = 0
+        for rabbit in rabbits:
+            if l[j] <= rabbit.genetics.step_size < l[j + 1]:
+                proportion += 1
+        if proportion > 0:
+            entropy += -(proportion / population) * math.log2(proportion / population)
+    variance.append(entropy)
+    entropy = 0
+    l = np.linspace(0, 1, 20)
+    for j in range(19):
+        proportion = 0
+        for rabbit in rabbits:
+            if l[j] <= rabbit.genetics.thirst_resistance < l[j + 1]:
+                proportion += 1
+        if proportion > 0:
+            entropy += -(proportion / population) * math.log2(proportion / population)
+    variance.append(entropy)
+    entropy = 0
+    for j in range(19):
+        proportion = 0
+        for rabbit in rabbits:
+            if l[j] <= rabbit.genetics.hunger_resistance < l[j + 1]:
+                proportion += 1
+        if proportion > 0:
+            entropy += -(proportion / population) * math.log2(proportion / population)
+    variance.append(entropy)
+    entropy = 0
+    l = np.linspace(0, 4, 20)
+    for j in range(19):
+        proportion = 0
+        for rabbit in rabbits:
+            if l[j] <= rabbit.genetics.predator_fear < l[j + 1]:
+                proportion += 1
+        if proportion > 0:
+            entropy += -(proportion / population) * math.log2(proportion / population)
+    variance.append(entropy)
+    entropy = 0
+    l = np.linspace(80, 200, 20)
+    for j in range(19):
+        proportion = 0
+        for rabbit in rabbits:
+            if l[j] <= rabbit.genetics.vision < l[j + 1]:
+                proportion += 1
+        if proportion > 0:
+            entropy += -(proportion / population) * math.log2(proportion / population)
+    variance.append(entropy)
+    return variance
+
+
+def find_genetic_variation_fox_entropy(foxes: list) -> list:
+    variance = []
+    population = len(foxes)
+    entropy = 0
+    l = np.linspace(0, 5, 20)
+    for j in range(19):
+        proportion = 0
+        for fox in foxes:
+            if l[j] <= fox.genetics.mating_requirement < l[j + 1]:
+                proportion += 1
+        if proportion > 0:
+            entropy += -(proportion / population) * math.log2(proportion / population)
+    variance.append(entropy)
+    entropy = 0
+    l = np.linspace(20, 45, 20)
+    for j in range(19):
+        proportion = 0
+        for fox in foxes:
+            if l[j] <= fox.genetics.step_size < l[j + 1]:
+                proportion += 1
+        if proportion > 0:
+            entropy += -(proportion / population) * math.log2(proportion / population)
+    variance.append(entropy)
+    entropy = 0
+    l = np.linspace(0, 0.5, 20)
+    for j in range(19):
+        proportion = 0
+        for fox in foxes:
+            if l[j] <= fox.genetics.thirst_resistance < l[j + 1]:
+                proportion += 1
+        if proportion > 0:
+            entropy += -(proportion / population) * math.log2(proportion / population)
+    variance.append(entropy)
+    entropy = 0
+    for j in range(19):
+        proportion = 0
+        for fox in foxes:
+            if l[j] <= fox.genetics.hunger_resistance < l[j + 1]:
+                proportion += 1
+        if proportion > 0:
+            entropy += -(proportion / population) * math.log2(proportion / population)
+    variance.append(entropy)
+    entropy = 0
+    l = np.linspace(130, 280, 20)
+    for j in range(19):
+        proportion = 0
+        for fox in foxes:
+            if l[j] <= fox.genetics.hunting_skill < l[j + 1]:
+                proportion += 1
+        if proportion > 0:
+            entropy += -(proportion / population) * math.log2(proportion / population)
+    variance.append(entropy)
+    entropy = 0
+    l = np.linspace(100, 280, 20)
+    for j in range(19):
+        proportion = 0
+        for fox in foxes:
+            if l[j] <= fox.genetics.vision < l[j + 1]:
+                proportion += 1
+        if proportion > 0:
+            entropy += -(proportion / population) * math.log2(proportion / population)
+    variance.append(entropy)
+    return variance
+
+
+def plot_variance_rabbit(variance, title):
     variance = np.array(variance).transpose()
     x_coordinates = range(1, len(variance[0]) + 1)
-    fig, part = plt.subplots(2,3)
+    fig, part = plt.subplots(2, 3)
     part[0][0].plot(x_coordinates, variance[0], color='#0bd6cc')
     part[0][0].set_title('Mating Requirement')
     part[0][1].plot(x_coordinates, variance[1], color='#a70bd6')
@@ -122,10 +429,11 @@ def plot_variance_rabbit(variance):
     part[1][1].set_title('Predator Fear')
     part[1][2].plot(x_coordinates, variance[5], color='#17453f')
     part[1][2].set_title('Vision Radius')
+    plt.suptitle(title)
     plt.show()
 
 
-def plot_variance_fox(variance):
+def plot_variance_fox(variance, title):
     variance = np.array(variance).transpose()
     x_coordinates = range(1, len(variance[0]) + 1)
     fig, part = plt.subplots(2, 3)
@@ -141,4 +449,5 @@ def plot_variance_fox(variance):
     part[1][1].set_title('Hunting Skill')
     part[1][2].plot(x_coordinates, variance[5], color='#17453f')
     part[1][2].set_title('Vision Radius')
+    plt.suptitle(title)
     plt.show()
